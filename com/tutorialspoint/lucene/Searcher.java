330a4a2cfa8ba470dfafc6017c031c346236b17a
Amir <amirabr@gmail.com>
2015-08-27 20:09:01 +0300

    added documentation to Searcher

--- com/tutorialspoint/lucene/Searcher.java
+++ com/tutorialspoint/lucene/Searcher.java
@@ -18,34 +18,72 @@ import org.apache.lucene.util.Version;
 
 public class Searcher {
 
-	IndexSearcher indexSearcher;
-	QueryParser queryParser;
-	Query query;
+	IndexSearcher indexSearcher; 	// Implements search over an index
+	QueryParser queryParser; 		// Parses a user-given query 
+	Query query; 					// The parsed query, to be used by the index searcher
 
+	/**
+	 * Constructor.
+	 * 
+	 * @param indexDirectoryPath
+	 * @throws IOException
+	 */
 	@SuppressWarnings("deprecation")
 	public Searcher(String indexDirectoryPath) throws IOException {
 		
+		// Open the directory where the index is saved
 		Directory indexDirectory = FSDirectory.open(new File(indexDirectoryPath));
+		
+		// Initialize the index searcher
 		indexSearcher = new IndexSearcher(indexDirectory);
-		queryParser = new QueryParser(Version.LUCENE_36, LuceneConstants.CONTENTS, new StandardAnalyzer(Version.LUCENE_36));
+		
+		// Initialize the query parser
+		queryParser = new QueryParser(Version.LUCENE_36,
+									  LuceneConstants.CONTENTS,
+									  new StandardAnalyzer(Version.LUCENE_36));
 		
 	}
 
-	public TopDocs search( String searchQuery) throws IOException, ParseException {
+	/**
+	 * Searches the index with the given query, returning the top search results.
+	 * 
+	 * @param searchQuery
+	 * @return
+	 * @throws IOException
+	 * @throws ParseException
+	 */
+	public TopDocs search(String searchQuery) throws IOException, ParseException {
       
+		// Parse the query
 		query = queryParser.parse(searchQuery);
+		
+		// Search the index
 		return indexSearcher.search(query, LuceneConstants.MAX_SEARCH);
 		
 	}
 
+	/**
+	 * Retrieve a document from the search results.
+	 * 
+	 * @param scoreDoc
+	 * @return
+	 * @throws CorruptIndexException
+	 * @throws IOException
+	 */
 	public Document getDocument(ScoreDoc scoreDoc) throws CorruptIndexException, IOException {
 		
 		return indexSearcher.doc(scoreDoc.doc);
 		
 	}
 
+	/**
+	 * Closes the index searcher.
+	 * 
+	 * @throws IOException
+	 */
 	public void close() throws IOException {
 		
+		// Close the index searcher
 		indexSearcher.close();
 		
 	}
